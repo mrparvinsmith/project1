a0a62c948c51a0712ef1c63f0e74c33db80aa4c6
Matthew Parvinsmith <mrparvinsmith@gmail.com>
2016-03-21 11:59:46 -0700

    improved make board function and associated css

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Determines how many cards are used in play; currently set as 16
 var playArray = [];
-var fullArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10,];
+var fullArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12];
 var numOfCards = 16;
 var cutDeck = function(){
   for(var i = 0; i < numOfCards; i++){
@@ -26,19 +26,19 @@ var makeBoard = function(){
   shuffle();
   for(var i = 0; i < (playArray.length - 3); i += 4){
     var $card1 = $('<div>');
-    $card1.addClass('hidden');
+    $card1.addClass('hidden card');
     $card1.text(playArray[i]);
     $('.top').append($card1);
     var $card2 = $('<div>');
-    $card2.addClass('hidden');
+    $card2.addClass('hidden card');
     $card2.text(playArray[i + 1]);
     $('.mid-top').append($card2);
     var $card3 = $('<div>');
-    $card3.addClass('hidden');
+    $card3.addClass('hidden card');
     $card3.text(playArray[i + 2]);
     $('.mid-bottom').append($card3);
     var $card4 = $('<div>');
-    $card4.addClass('hidden');
+    $card4.addClass('hidden card');
     $card4.text(playArray[i + 3]);
     $('.bottom').append($card4);
   }
